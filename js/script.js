1a4afe8f95b51ac29fbcce74e165f50bbc10dbb4
Andrea Esposito <andexpox@gmail.com>
2021-04-27 16:50:56 +0200

    list-email

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 var app = new Vue ({
     el: '#root',
     data: {
-
+        listEmailLength: 10,
+        listEmail: []
     },
     methods: {
 
     },
+    mounted() {
+        for (let i = 0; i < this.listEmailLength; i++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    const result = response.data.response;
+                    this.listEmail.push(result);
+            });
+        }
+
+        console.log(this.listEmail);
+    }
 });
